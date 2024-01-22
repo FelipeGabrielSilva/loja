c9c6d5871f23d6c7461d9b9cae47ac234caf5bf3
Felipe <felipegabriel1106@outlook.com>
2024-01-22 12:13:53 -0300

    Selecionando informações a serem exibidas

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,19 +1,40 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
 import { createUsuarioDTO } from "./dto/createUsuario.dto";
+import { ListaUsuarioDTO } from "./dto/listaUsuario.dto";
+import { UsuarioEntity } from "./entities/usuario.entity";
+import { v4 as uuid } from "uuid";
 
 @Controller('/usuarios')
 export class UsuarioController {
     constructor(private usuarioRepository: UsuarioRepository) { }
 
     @Post()
-    async criarUsuario(@Body() dadosUsuarios: createUsuarioDTO) {
-        this.usuarioRepository.salvar(dadosUsuarios);
-        return dadosUsuarios;
+    async criaUsuario(@Body() dadosDoUsuario: createUsuarioDTO) {
+        const usuarioEntity = new UsuarioEntity();
+        usuarioEntity.email = dadosDoUsuario.email;
+        usuarioEntity.senha = dadosDoUsuario.senha;
+        usuarioEntity.nome = dadosDoUsuario.nome;
+        usuarioEntity.id = uuid();
+
+        this.usuarioRepository.salvar(usuarioEntity);
+
+        return {
+            usuario: new ListaUsuarioDTO(usuarioEntity.id, usuarioEntity.nome),
+            mensagem: `Usuário ${usuarioEntity.nome} criado com sucesso`,
+        };
     }
 
     @Get()
-    async listaUsuarios() {
-        return this.usuarioRepository.listar();
+    async listUsuarios() {
+        const usuariosSalvos = await this.usuarioRepository.listar();
+        const usuariosLista = usuariosSalvos.map(
+            usuario => new ListaUsuarioDTO(
+                usuario.id,
+                usuario.nome
+            )
+        );
+
+        return usuariosLista;
     }
 }
